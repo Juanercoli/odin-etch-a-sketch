8ac03ea046823d00b06c22f15e07e539f376b9b5
Juan Ercoli <ercoli.juan@alu.ing.unlp.edu.ar>
2023-12-28 01:21:01 -0300

    Added createGrid function

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 let container = document.querySelector("#grid-container");
-const gridContainerSize = parseInt(window.getComputedStyle(container).getPropertyValue('max-width'));
-const gridSize = 16;
-// Create rows
-for (let i = 0; i < gridSize; i++) {
-  let row = document.createElement("div");
-  row.className = "grid-row";
-  // Create columns
+createGrid(16);
+
+/* Create grid container */
+function createGrid(gridSize) {
+  // Get container size
+  const gridContainerSize = parseInt(
+    window.getComputedStyle(container).getPropertyValue("max-width")
+  );
+
+  // Create rows
   for (let i = 0; i < gridSize; i++) {
-    let column = document.createElement("div");
-    column.className = "grid-column";
-    column.style['width'] = gridContainerSize/gridSize + 'px';
-    column.style['height'] = gridContainerSize/gridSize + 'px';
-    row.append(column);
+    let row = document.createElement("div");
+    row.className = "grid-row";
+    // Create columns
+    for (let i = 0; i < gridSize; i++) {
+      let column = document.createElement("div");
+      column.className = "grid-column";
+      column.style["width"] = gridContainerSize / gridSize + "px";
+      column.style["height"] = gridContainerSize / gridSize + "px";
+      row.append(column);
+    }
+    container.append(row);
   }
-  container.append(row);
 }
